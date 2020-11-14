099f2a406d211b4c9fc0a9a544437e0dc79c5268
Michael Penkov <misha.penkov@gmail.com>
2020-11-14 16:47:54 +0900

    add test-depends.txt

--- config.sh
+++ config.sh
@@ -1,5 +1,4 @@
 # Define custom utilities
-# Test for OSX with [ -n "$IS_OSX" ]
 
 function pre_build {
     # Any stuff that you need to do before you start building the wheels
@@ -10,9 +9,6 @@ function pre_build {
 function run_tests {
     # Runs tests on installed distribution from an empty directory
     python --version
-    if [[ "$PLAT" -ne "i686" ]]; then
-        pip install "tensorflow<=1.3.0" "keras>=2.0.4"  # additional deps, available only for x64
-    fi
     pip freeze
-    pytest -rfxEXs --durations=20 --disable-warnings --showlocals --reruns 3 --reruns-delay 1 --pyargs gensim
+    pytest -rfxEXs --durations=20 --disable-warnings --showlocals --pyargs gensim
 }
